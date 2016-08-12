041e062ad9296a2eacc17e5e0a43f28312cd3bd8
Rommel Berrios <rommel.berrios93@gmail.com>
2016-08-12 00:29:59 -0600

    Define index for platforms

--- index.android.js
+++ index.android.js
@@ -1,53 +1,81 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
-
 import React, { Component } from 'react';
+
 import {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
+  BackAndroid,
+  Navigator,
   Text,
-  View
+  TouchableOpacity,
+  View,
 } from 'react-native';
 
-class AwesomeProject extends Component {
-  render() {
+var HomePage = require('./src/home_page')
+var SplashPage = require('./src/splash_page')
+var ViewTopic = require('./src/view_topic')
+
+var _navigator;
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+    _navigator.pop();
+    return true;
+  }
+  return false;
+});
+
+class App extends Component {
+render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Rommel!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R asdkeyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <Navigator
+          initialRoute={{id: 'SplashPage', name: 'Index'}}
+          renderScene={this.renderScene.bind(this)}
+          configureScene={(route) => {
+            if (route.sceneConfig) {
+              return route.sceneConfig;
+            }
+            return Navigator.SceneConfigs.FloatFromRight;
+          }} />
+    );
+  }
+    renderScene(route, navigator) {
+    var {id, bundle} = route;
+    _navigator = navigator
+    switch (id){
+      case "SplashPage":
+        return (
+          <SplashPage
+          navigator={navigator}/>
+        );
+      case "HomePage":
+        return (
+          <HomePage
+            navigator={navigator} />
+        );
+      case "ViewTopic":
+        return (
+          <ViewTopic
+            navigator={navigator} bundle={bundle} />
+        )
+        default:
+          //return this.noRoute(navigator);
+      }
+  }
+  noRoute(navigator) {
+    return (
+      <View style={{flex: 1, alignItems: 'stretch', justifyContent: 'center'}}>
+        <TouchableOpacity style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+            onPress={() => navigator.pop()}>
+          <Text style={{color: 'red', fontWeight: 'bold'}}>è¯·åœ¨ index.js çš„ renderScene ä¸­é…ç½®è¿™ä¸ªé¡µé¢çš„è·¯ç”±</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => App);
